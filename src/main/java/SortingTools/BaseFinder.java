4dc3a43fedbc358e003c4741b47fc9367c8047bf
TheEddy <edr0981@gmail.com>
2016-09-10 22:24:46 +0300

    Primitive TCPServer/Client added Site project added

--- src/main/java/SortingTools/BaseFinder.java
+++ src/main/java/SortingTools/BaseFinder.java
@@ -8,8 +8,13 @@ import java.sql.*;
  * Created by Fedor on 05-Sep-16.
  */
 public class BaseFinder {
+    public static void main(String[] args){
+        String database = "carsystem";
+        String car_number = "В887МХ177";
 
-    public BaseFinder(String car_number) throws SQLException {
+    }
+
+    public boolean BaseFinder(String car_number) throws SQLException {
         DatabaseConnection databaseConnection = new DatabaseConnection();
         Connection connection = databaseConnection.getConnection();
         String search = "\"\'" + car_number + "\'\"";
@@ -18,21 +23,33 @@ public class BaseFinder {
         Statement statement;
         statement = connection.createStatement();
         ResultSet rs = statement.executeQuery(SELECT_NUMBER);
-        if (rs.next()) {
-            Integer car_id;
+        try {
+            rs.next();
+            /*Integer car_id;
             String car_numbers;
             Integer car_accident;
-            //Записываем значения из БД в переменные
-            rs.next(); // Выбирает найденое значение.
             car_id = rs.getInt(1);
             car_numbers = rs.getString(2);
             car_accident = rs.getInt(3);
             System.out.println(
                     "ID автомобиля в БД:   " +  car_id +
                             "\nГос. Номер автомобиля:" + car_numbers +
-                            "\nКоличество ДТП:       " + car_accident);
+                            "\nКоличество ДТП:       " + car_accident);*/
+            return true;
         }
+        catch (SQLException e) {
+            return false;
+        }
+        /*if (rs.next()) {
 
+            //Записываем значения из БД в переменные
+            rs.next(); // Выбирает найденое значение.
+
+
+
+        }*/
+
+        //return false;
     }
 
 }
---
+++ src/main/java/TCPServer.java
@@ -0,0 +1,37 @@
+/**
+ * Created by Fedor on 10.09.2016 19:03.
+ */
+
+import java.io.*;
+import java.net.*;
+import java.nio.charset.Charset;
+
+class TCPServer
+{
+    public static void main(String argv[]) throws Exception
+    {
+        String clientSentence;
+        String capitalizedSentence;
+        ServerSocket welcomeSocket = new ServerSocket(6789);
+
+        while(true)
+        {
+            Socket connectionSocket = welcomeSocket.accept();
+            Charset inputCharset = Charset.forName("UTF8");
+
+            BufferedReader inFromClient =
+                    new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+            clientSentence = inFromClient.readLine().replace("\u000E", "");
+            clientSentence = inFromClient.readLine().replace("/", "");
+            System.out.println("Получен номер: " + clientSentence);
+
+
+            capitalizedSentence = "Номер принят к записи" + '\n';
+            //outToClient.writeBytes(capitalizedSentence);
+            outToClient.writeUTF(capitalizedSentence);
+        }
+    }
+}
