78fe472d6dbfeacbf5043f81e095d691c763d1cb
TheEddy <edr0981@gmail.com>
2016-09-28 21:58:51 +0300

    mail.php re-writed again TCPServer log strings added
    TODO list updated(Russian)

--- src/main/java/TCPServer.java
+++ src/main/java/TCPServer.java
@@ -24,17 +24,25 @@ class TCPServer
             System.out.println("Client accepted: " + connectionSocket);
             Charset inputCharset = Charset.forName("UTF8");
 
+            System.out.println("Состояние 1");
             BufferedReader inFromClient =
                     new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
 
+            System.out.println("Состояние 2");
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
+            System.out.println("Состояние 3");
             boolean connected = true;
             while (connected) {
                 try {
-                    clientSentence = inFromClient.readLine().replace("\u000E", "");
+                    System.out.println("Состояние 4");
+                    clientSentence = inFromClient.readLine();
+                    /*clientSentence = inFromClient.readLine().replace("\u000E", "");
+                    System.out.println("Состояние 5" + clientSentence);
                     clientSentence = inFromClient.readLine().replace("/", "");
-                    System.out.println("Получен номер: " + clientSentence.replace(".", ""));
+                    System.out.println("Состояние 6" + clientSentence);
+                    System.out.println("Получен номер: " + clientSentence.replace(".", ""));*/
+                    System.out.println("Получен номер: " + clientSentence);
                     capitalizedSentence = "Номер принят к записи" + '\n';
                     //outToClient.writeBytes(capitalizedSentence);
                     outToClient.writeUTF(capitalizedSentence);
--- src/main/java/minimal site/mail.php
+++ src/main/java/minimal site/mail.php
@@ -1,35 +1,51 @@
 <?php
 
 set_time_limit(0);
-//$number = trim($_POST["number"]);
-//$time = trim($_POST["time"]);
-//$text = trim($_POST["text"]);
-$address = '127.0.0.1';
+$number = trim($_POST["number"]);
+$number = $number . PHP_EOL;
+print($number);
+$time = trim($_POST["time"]);
+$text = trim($_POST["text"]);
+$address = "localhost";
 $port = 6789;
 $sockets = array();
-$number = 1;
+//$number = 'Ю124ЮА99' . PHP_EOL;
+$errstr = '';
+$errno = '';
 
+/*if ( ($socket = fsockopen($address, $port, $errno, $errstr, 3) ) === FALSE)
+    echo "$errstr ($errno)";
+else {
+    print "Номер отправлен";
+    fwrite($socket, $number);
+    while (! feof($socket)) {
+        echo fgets($socket, 4096);
+    }
+    fclose($socket);
+}
+*/
 if(!empty($number)) {
+    print($number);
     $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
     if ($socket === false) {
         echo "Не удалось инициализировать сокет: " . socket_strerror(socket_last_error()) . "\n";
     } else {
         echo "Инициализация сокета... OK.\n";
     }
-    echo "Попытка подключения к '$address' : '$port'...";
+    echo "Попытка подключения к $address:$port...";
     $result = socket_connect($socket, $address, $port);
     if ($result === false) {
         echo "Подключение не удалось: ($result) " . socket_strerror(socket_last_error($socket)) . "\n";
     } else {
         echo "Подключение... OK.\n";
     }
-    $number = "Р956МЕ99"; //Удали эту строку и расскоментируй первые 3, если собираешься работать через браузер.
+    //$number = 'А124АА99' . PHP_EOL; //Удали эту строку и расскоментируй первые 3, если собираешься работать через браузер.
     echo "Отправка номера...\n";
-    $number_byte = utf8_encode ($number);
+    //$number_byte = utf8_encode ($number);
     //echo $number;
-    echo $number_byte;
+    echo $number;
     echo "\n";
-    socket_write($socket, $number_byte, strlen($number_byte)); //Процесс отправки информации. Проблема: информация не доходит.
+    socket_write($socket, $number, strlen($number)); //Процесс отправки информации. Проблема: информация не доходит.
     echo "Отправка произведена.\n";
     echo "Ожидание ответа:\n\n";
     while ($out = socket_read($socket, 2048)) {
